1dfb674a4ff4fb9c237c8926c6b12fe4b056e86c
Danny Yoo <dyoo@cs.wpi.edu>
2011-06-10 17:13:41 -0400

    hello world

--- lang/reader.rkt
+++ lang/reader.rkt
@@ -4,15 +4,19 @@
 #:read-syntax my-read-syntax
 
 
-(require "parser.rkt")
 
 ;; Now that we have a parser, let't provide the two functions that a
-;; language reader module needs to define.
+;; language reader module needs to define.  These are the read and
+;; read-syntax functions.
+
+(require "parser.rkt")
+
 
 ;; my-read: input-port -> s-expression
 (define (my-read in)
   (syntax->datum
    (my-read-syntax #f in)))
+
 
 
 ;; my-read-syntax: any input-port -> syntax-object
@@ -21,6 +25,9 @@
   ;; Note: we're ignoring our src, but if our parser were a bit nicer,
   ;; it would also keep track of the src in the resulting syntax
   ;; objects.
+
+  ;; We also need to return eof once we've exhausted the token stream,
+  ;; so let's do so.
   (let ([token-stream (get-tokens in)])
     (cond
      [(eof-object? (peek token-stream))
