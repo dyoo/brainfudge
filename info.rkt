ceac1c951e40b7aa3a6b904f9c8066ee51cbcd1d
Danny Yoo <dyoo@cs.wpi.edu>
2011-06-20 12:57:04 -0400

    updating the version to 1.6

--- info.rkt
+++ info.rkt
@@ -3,11 +3,11 @@
 (define categories '(devtools))
 (define can-be-loaded-with 'all)
 (define required-core-version "5.1.1")
-(define version "1.5")
+(define version "1.6")
 (define repositories '("4.x"))
 (define scribblings '(("manual.scrbl" () (getting-started))))
 (define primary-file "language.rkt")
 (define blurb 
   '("bf: a brainf*ck compiler for Racket.  Includes a tutorial for building a language in Racket."))
 (define release-notes
-  '((p "Light fixes to the tutorial.")))
+  '((p "Internal changes to make the generated code run faster.")))
