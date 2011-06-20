ec9342e43fd2d941641abcbd582b10027b08d3cb
Danny Yoo <dyoo@cs.wpi.edu>
2011-06-20 17:06:04 -0400

    bumping to 1.7

--- info.rkt
+++ info.rkt
@@ -3,11 +3,11 @@
 (define categories '(devtools))
 (define can-be-loaded-with 'all)
 (define required-core-version "5.1.1")
-(define version "1.6")
+(define version "1.7")
 (define repositories '("4.x"))
 (define scribblings '(("manual.scrbl" () (getting-started))))
 (define primary-file "language.rkt")
 (define blurb 
   '("bf: a brainf*ck compiler for Racket.  Includes a tutorial for building a language in Racket."))
 (define release-notes
-  '((p "Internal changes to make the generated code run faster.")))
+  '((p "More internal changes to make the generated code run faster.  The internal representation defines the state in two pieces, and also uses racket/unsafe/ops.")))
