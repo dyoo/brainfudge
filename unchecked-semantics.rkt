fe27960433dd69d9ac278b84a9e7945919af01fc
Danny Yoo <dyoo@cs.wpi.edu>
2011-06-20 16:55:43 -0400

    changing the semantics so the data and ptr are defined separately

--- unchecked-semantics.rkt
+++ unchecked-semantics.rkt
@@ -1,8 +1,12 @@
 #lang racket
 
-;; This is a second semantics for the language that tries to go for speed.
+;; This is a second semantics for the language that tries to go for speed,
+;; at the expense of making things a little more complicated.
+;;
 ;; It uses features in: http://docs.racket-lang.org/reference/unsafe.html
 ;; to reduce the number of runtime checks.
+;;
+;; We also manage the state as two separate values.
 
 (require rackunit               ;; we want unit tests
          racket/unsafe/ops      ;; and we want raw, unsafe access for speed
@@ -12,27 +16,12 @@
 (provide (all-defined-out))
 
 
-;; Our state includes two pieces.
-(define-struct state (data ptr)
-  #:mutable)
-
-
-(define-syntax-rule (unsafe-state-data a-state)
-  (unsafe-struct-ref a-state 0))
-
-(define-syntax-rule (unsafe-state-ptr a-state)
-  (unsafe-struct-ref a-state 1))
-
-(define-syntax-rule (unsafe-set-state-ptr! a-state v)
-  (unsafe-struct-set! a-state 1 v))
-
-
 
 ;; Creates a new state, with a byte array of 30000 zeros, and
 ;; the pointer at index 0.
 (define (new-state) 
-  (make-state (make-vector 30000 0)
-              0))
+  (values (make-vector 30000 0)
+          0))
 
 
 ;; Check to see if we've gone out of range.  If we have a useful stx
@@ -44,54 +33,45 @@
 
 
 ;; increment the data pointer
-(define-syntax-rule (increment-ptr a-state stx)
+(define-syntax-rule (increment-ptr data ptr stx)
   (begin
-    (unsafe-set-state-ptr! a-state (unsafe-fx+ (unsafe-state-ptr a-state) 1))
-    (when (unsafe-fx>= (unsafe-state-ptr a-state) (unsafe-vector-length (unsafe-state-data a-state)))
+    (set! ptr (unsafe-fx+ ptr 1))
+    (when (unsafe-fx>= ptr (unsafe-vector-length data))
       (raise-range-errors! a-state 'increment-ptr stx))))
 
 
 ;; decrement the data pointer
-(define-syntax-rule (decrement-ptr a-state stx)
+(define-syntax-rule (decrement-ptr data ptr stx)
   (begin
-    (unsafe-set-state-ptr! a-state (unsafe-fx- (unsafe-state-ptr a-state) 1))
-    (when (unsafe-fx< (unsafe-state-ptr a-state) 0)
+    (set! ptr (unsafe-fx- ptr 1))
+    (when (unsafe-fx< ptr 0)
       (raise-range-errors! a-state 'decrement-ptr stx))))
 
 
 ;; increment the byte at the data pointer
-(define-syntax-rule (increment-byte a-state)
-  (let ([v (unsafe-state-data a-state)]
-        [i (unsafe-state-ptr a-state)])
-    (unsafe-vector-set! v i (unsafe-fx+ (unsafe-vector-ref v i) 1))))
+(define-syntax-rule (increment-byte data ptr)
+  (unsafe-vector-set! data ptr (unsafe-fx+ (unsafe-vector-ref data ptr) 1)))
 
 ;; decrement the byte at the data pointer
-(define-syntax-rule (decrement-byte a-state)
-  (let ([v (unsafe-state-data a-state)]
-        [i (unsafe-state-ptr a-state)])
-    (unsafe-vector-set! v i (unsafe-fx- (unsafe-vector-ref v i) 1))))
+(define-syntax-rule (decrement-byte data ptr)
+  (unsafe-vector-set! data ptr (unsafe-fx- (unsafe-vector-ref data ptr) 1)))
 
 ;; print the byte at the data pointer
-(define-syntax-rule (write-byte-to-stdout a-state)
-  (let ([v (unsafe-state-data a-state)]
-        [i (unsafe-state-ptr a-state)])
-    (write-byte (unsafe-vector-ref v i) (current-output-port))))
+(define-syntax-rule (write-byte-to-stdout data ptr)
+  (write-byte (unsafe-vector-ref data ptr) (current-output-port)))
 
 ;; read a byte from stdin into the data pointer
-(define-syntax-rule (read-byte-from-stdin a-state)
-  (let ([v (unsafe-state-data a-state)]
-        [i (unsafe-state-ptr a-state)])
-    (unsafe-vector-set! v i (let ([v (read-byte (current-input-port))])
-                              (if (eof-object? v)
-                                  0
-                                  v)))))
+(define-syntax-rule (read-byte-from-stdin data ptr)
+  (unsafe-vector-set! data ptr (let ([v (read-byte (current-input-port))])
+                                 (if (eof-object? v)
+                                     0
+                                     v))))
 
 ;; we know how to do loops!
-(define-syntax-rule (loop a-state body ...)
+(define-syntax-rule (loop data ptr body ...)
   (let loop ()
-    (unless (unsafe-fx= (unsafe-vector-ref (unsafe-state-data a-state)
-                                  (unsafe-state-ptr a-state))
-               0)
+    (unless (unsafe-fx= (unsafe-vector-ref data ptr)
+                        0)
       body ...
       (loop))))
 
@@ -102,62 +82,61 @@
 
 
 ;; Simple exercises.
-(let ([s (new-state)])
-  (increment-byte s)
-  (check-equal? 1 (vector-ref (state-data s) 0))
-  (increment-byte s)
-  (check-equal? 2 (vector-ref (state-data s) 0))
-  (decrement-byte s)
-  (check-equal? 1 (vector-ref (state-data s) 0)))
+(let-values ([(data ptr) (new-state)])
+  (increment-byte data ptr)
+  (check-equal? 1 (vector-ref data 0))
+  (increment-byte data ptr)
+  (check-equal? 2 (vector-ref data 0))
+  (decrement-byte data ptr)
+  (check-equal? 1 (vector-ref data 0)))
 
 ;; pointer movement
-(let ([s (new-state)])
-  (increment-ptr s #f)
-  (increment-byte s)
-  (check-equal? 0 (vector-ref (state-data s) 0))
-  (check-equal? 1 (vector-ref (state-data s) 1))
-  (decrement-ptr s #f)
-  (increment-byte s)
-  (check-equal? 1 (vector-ref (state-data s) 0))
-  (check-equal? 1 (vector-ref (state-data s) 1)))
+(let-values ([(data ptr) (new-state)])
+  (increment-ptr data ptr #f)
+  (increment-byte data ptr)
+  (check-equal? 0 (vector-ref data 0))
+  (check-equal? 1 (vector-ref data 1))
+  (decrement-ptr data ptr #f)
+  (increment-byte data ptr)
+  (check-equal? 1 (vector-ref data 0))
+  (check-equal? 1 (vector-ref data 1)))
 
 ;; make sure standard input is doing something
-(let ([s (new-state)])
+(let-values ([(data ptr) (new-state)])
   (parameterize ([current-input-port
                   (open-input-bytes (bytes 3 1 4))])
-    (read-byte-from-stdin s)
-    (increment-ptr s #f)
-    (read-byte-from-stdin s)
-    (increment-ptr s #f)
-    (read-byte-from-stdin s))
-  (check-equal? 3 (vector-ref (state-data s) 0))
-  (check-equal? 1 (vector-ref (state-data s) 1))
-  (check-equal? 4 (vector-ref (state-data s) 2)))
+    (read-byte-from-stdin data ptr)
+    (increment-ptr data ptr #f)
+    (read-byte-from-stdin data ptr)
+    (increment-ptr data ptr #f)
+    (read-byte-from-stdin data ptr))
+  (check-equal? 3 (vector-ref data 0))
+  (check-equal? 1 (vector-ref data 1))
+  (check-equal? 4 (vector-ref data 2)))
 
 
 ;; make sure standard output is doing something
-(let ([s (new-state)])
-  (set-state-data! s (vector 80 76 84))
+(let-values ([(data ptr) (new-state)])
+  (set! data (vector 80 76 84))
   (let ([simulated-stdout (open-output-string)])
     (parameterize ([current-output-port simulated-stdout])
-      (write-byte-to-stdout s)
-      (increment-ptr s #f)
-      (write-byte-to-stdout s)
-      (increment-ptr s #f)
-      (write-byte-to-stdout s))
+      (write-byte-to-stdout data ptr)
+      (increment-ptr data ptr #f)
+      (write-byte-to-stdout data ptr)
+      (increment-ptr data ptr #f)
+      (write-byte-to-stdout data ptr))
     (check-equal? "PLT" (get-output-string simulated-stdout))))
 
 
 ;; Let's see that we can clear.
-(let ([s (new-state)])
-  (set-state-data! s (vector 0 104 101 108 112 109 101 105
-                             109 109 101 108 116 105 110 103 ))
-  (set-state-ptr! s 15)
+(let-values ([(data ptr) (new-state)])
+  (set! data (vector 0 104 101 108 112 109 101 105
+                     109 109 101 108 116 105 110 103 ))
+  (set! ptr 15)
   ;; [ [-] < ]
-  (loop s 
-        (loop s (decrement-byte s))
-        (decrement-ptr s #f))
+  (loop data ptr 
+        (loop data ptr (decrement-byte data ptr))
+        (decrement-ptr data ptr #f))
   
-  (check-equal? 0 (state-ptr s))
-  (check-equal? (make-vector 16 0) (state-data s)))
-                 
+  (check-equal? 0 ptr)
+  (check-equal? (make-vector 16 0) data))
