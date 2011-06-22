fa8d10c4fe50d97b26cfb97bbf099c400de8fc07
Danny Yoo <dyoo@cs.wpi.edu>
2011-06-22 11:41:36 -0400

    cooperating more with drracket by telling it how to color lex us

--- lang/reader.rkt
+++ lang/reader.rkt
@@ -2,7 +2,7 @@
 (planet dyoo/bf/unchecked-language)   ;; switched from (planet dyoo/bf/language)
 #:read my-read
 #:read-syntax my-read-syntax
-
+#:info my-get-info
 (require "../parser.rkt")
 
 (define (my-read in)
@@ -10,3 +10,21 @@
 
 (define (my-read-syntax src in)
   (parse-expr src in))
+
+
+
+;; Extension: we'd like to cooperate with DrRacket and tell
+;; it to use the default, textual lexer and color scheme when
+;; editing bf programs.
+;;
+;; See: http://docs.racket-lang.org/guide/language-get-info.html
+;; for more details, as well as the documentation in
+;; syntax/module-reader.
+(define (my-get-info key default default-filter)
+  (case key
+    [(color-lexer)
+     (dynamic-require 'syntax-color/default-lexer
+                      'default-lexer)]
+    [else
+     (default-filter key default)]))
+
